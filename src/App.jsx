d3922e9b1540e6094cfb46e03b747165c3fecaba
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-05-03 23:23:13 -0400

    Création de cubes dans un canvas

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
+import { Canvas } from "@react-three/fiber";
+import PropTypes from "prop-types";
 import "./App.css";
+
+export const Cube = (props) => {
+  return (
+    <mesh position={props.position}>
+      <boxGeometry />
+      <meshStandardMaterial color={"orange"} />
+    </mesh>
+  );
+};
+
+Cube.propTypes = {
+  position: PropTypes.array,
+};
 
 const App = () => {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <Canvas>
+      <directionalLight position={[0, 0, 2]} />
+
+      <Cube position={[1, 0, 0]} />
+      <Cube position={[-1, 0, 0]} />
+      <Cube position={[1, 2, 0]} />
+      <Cube position={[-1, 2, 0]} />
+    </Canvas>
   );
 };
 
